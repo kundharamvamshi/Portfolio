fa90bf8ce6f901271e2e51c5c2a5c4188f5a9dff
kundharamvamshi <156309354+kundharamvamshi@users.noreply.github.com>
2026-02-13 03:13:07 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
-const themeToggle = document.getElementById("theme-toggle");
-const body = document.body;
-const storedTheme = localStorage.getItem("theme");
-
-if (storedTheme === "dark") {
-  body.classList.add("dark");
-}
-
-function setThemeButtonText() {
-  const isDark = body.classList.contains("dark");
-  themeToggle.textContent = isDark ? "Light" : "Dark";
-}
-
-setThemeButtonText();
-
-themeToggle.addEventListener("click", () => {
-  body.classList.toggle("dark");
-  const isDark = body.classList.contains("dark");
-  localStorage.setItem("theme", isDark ? "dark" : "light");
-  setThemeButtonText();
+window.addEventListener("scroll", () => {
+  var header = document.querySelector("header");
+  header.classList.toggle("topbar-onscroll", window.scrollY > 0);
 });
-
-document.getElementById("year").textContent = new Date().getFullYear();
