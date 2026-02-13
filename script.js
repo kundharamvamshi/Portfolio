c86ca3c36bd3f3d968cd80e55123d89d4550ab98
kundharamvamshi <156309354+kundharamvamshi@users.noreply.github.com>
2026-02-13 23:13:39 +0530

    initial commit

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ window.addEventListener("scroll", () => {
   var header = document.querySelector("header");
   header.classList.toggle("topbar-onscroll", window.scrollY > 0);
 });
+
+function hidesidebar(){
+  var sidebarEl=document.querySelector('.sidebar')
+  sidebarEl.style.display='none'
+}
+
+function showsidebar(){
+  var sidebarEl=document.querySelector('.sidebar')
+  sidebarEl.style.display='flex'
+}
