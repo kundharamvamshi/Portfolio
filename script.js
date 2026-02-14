5481ca368e08d979df466c022383377e0867a944
kundharamvamshi <156309354+kundharamvamshi@users.noreply.github.com>
2026-02-14 23:55:34 +0530

    initial commit

--- script.js
+++ script.js
@@ -3,12 +3,16 @@ window.addEventListener("scroll", () => {
   header.classList.toggle("topbar-onscroll", window.scrollY > 0);
 });
 
-function hidesidebar(){
-  var sidebarEl=document.querySelector('.sidebar')
-  sidebarEl.style.display='none'
+function hidesidebar() {
+  var sidebarEl = document.querySelector('.sidebar')
+  sidebarEl.style.display = 'none'
 }
 
-function showsidebar(){
-  var sidebarEl=document.querySelector('.sidebar')
-  sidebarEl.style.display='flex'
+function showsidebar() {
+  var sidebarEl = document.querySelector('.sidebar')
+  sidebarEl.style.display = 'flex'
 }
+
+const footerYear = document.getElementById('year')
+const presentYear = new Date().getFullYear()
+footerYear.textContent = " " + presentYear + " "
